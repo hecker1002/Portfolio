575864d23622883feba7e8be732bcb6421ac30a0
hecker1002 <polygot882@gmail.com>
2023-12-29 20:43:09 +0530

    changed style of portfolio

---
+++ Home.js
@@ -0,0 +1,40 @@
+// Array of strings to type
+const textArray = [
+    "Welcome! Let me take you to my recent work",
+    "Explore my Projects",
+    "Read my Blogs",
+    "And many more to come ..."
+];
+
+let index = 0; // Index of the current string
+let charIndex = 0; // Index of the current character
+
+function type() {
+    const typedTextElement = document.getElementById("typed-text");
+    const currentText = textArray[index];
+    const char = currentText[charIndex];
+
+    // Display the current character
+    typedTextElement.textContent += char;
+
+    charIndex++;
+
+    // Move to the next string when the current one is completed
+    if (charIndex === currentText.length) {
+        index++;
+        charIndex = 0;
+
+        // Reset the typed text for the next string after a short delay
+        setTimeout(() => {
+            typedTextElement.textContent = "";
+            // Start typing the next string after another short delay
+            setTimeout(type, 500);
+        }, 1000);
+    } else {
+        // Start typing the next character after a short delay
+        setTimeout(type, 100);
+    }
+}
+
+// Start the typing effect
+type();
